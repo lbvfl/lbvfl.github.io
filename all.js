171db1b780a8d6a6c15477f7cf13fe92af66c5e0
lbvfl <163737021+lbvfl@users.noreply.github.com>
2026-08-11 11:03:46 +0300

    Update all.js

--- all.js
+++ all.js
@@ -9,34 +9,27 @@
         [
 
           "http://wtch.ch/m", //Онлайн без преміум
+          "http://bwa.ad/rc", // Онлайн BWA
           "https://lampame.github.io/main/bo.js", // Бандера Онлайн
+
+
+         // "https://вашепосилання",
+         // "https://вашепосилання",
+         // "https://вашепосилання",
+         // "https://вашепосилання",
+
+         // "https://icantrytodo.github.io/lampa/torrent_styles_v2.js", //стиль торентів може конфліктувати з іншими стилями
+         // "https://darkestclouds.github.io/plugins/easytorrent/easytorrent.min.js", //рекомендація торрентів
+
           
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
+          "https://ko31k.github.io/LMP/plugins/Parsers.js",    // Каталог парсерів
+          "https://ko31k.github.io/LMP/plugins/interface+.js", // Комплексне покращення інтерфейсу: панелі, мітки, теми, керування кнопками
+          "https://ko31k.github.io/LMP/plugins/buttons+.js",   // Редактор кнопок в картці фільму/серіалу
+          "https://ko31k.github.io/LMP/plugins/cardify+.js",   // Широкий інтефейс для картки. Чистіша картка, фонові трейлери або слайдшоу
+          "https://apxubatop.github.io/lmpPlugs/tvbutton.js",  // Налаштування поведінки першої кнопки
+          "https://ko31k.github.io/LMP/plugins/menueditor.js"  // Налаштування меню
 
-          "https://вашепосилання",
 
-          // "https://вашепосилання",
-          // "https://вашепосилання",
-          // "https://вашепосилання,
-
-          "https://вашепосилання",
-
-          "http://вашепосилання",
-
-          "https://вашепосилання",
-
-          "https://icantrytodo.github.io/lampa/torrent_styles_v2.js", //стиль торентів може конфліктувати з іншими стилями
-          "https://darkestclouds.github.io/plugins/easytorrent/easytorrent.min.js", //рекомендація торрентів
-          "https://вашепосилання",
 
           
         ],
